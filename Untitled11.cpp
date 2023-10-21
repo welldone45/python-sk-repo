1a6074e565828bdb6f0ffda218924d27b0f3a8fb
welldone45 <sktech98799@gmail.com>
2023-10-21 08:32:43 +0530

    hello

---
+++ Untitled11.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+// pre processor
+// standard input output
+/*
+	.h header file
+	console
+	input
+	output
+	multi line comment
+
+
+
+*/
+
+main()
+{
+	getch();  // to get the character
+	clrscr(); // to clear the screen
+	printf("Welcome to lecture of C Language");
+	//printf is a function just like main is a function
+	return 0;
+}
+
+/*
+it is a rule that function should return a value
+
+*/
